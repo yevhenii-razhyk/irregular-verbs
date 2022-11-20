d9ae7cea5f369b0743f33a1806d4dfc62dd2117d
yevhenii-razhyk <yevhenii-razhyk@outlook.com>
2022-11-20 22:14:48 +0200

    add overflow for table

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,43 +8,49 @@ import Select from '../Select/Select';
 
 const App: React.FC = () => {
 
-  const verbs = verbsList;
-  const [selectedSort, setSelectedSort] = useState("v1");
-  const [selectedSearch, setSelectedSearch] = useState("v1");
-  const [searchQuerry, setSearchQuerry] = useState("")
+    const verbs = verbsList;
+    const [selectedSort, setSelectedSort] = useState("");
+    const [selectedSearch, setSelectedSearch] = useState("");
+    const [searchQuerry, setSearchQuerry] = useState("")
 
-  const sortList = (sort: string) => {
-    setSelectedSort(sort);
-  }
-
-  const sortedList = useMemo(() => {
-    if (selectedSort) {
-      return [...verbs].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
-    } else {
-      return verbs
+    const sortList = (sort: string) => {
+        setSelectedSort(sort);
     }
-  }, [selectedSort, verbs]);
 
-  const sortedAndSearchedList = useMemo(() => {
-    return [...sortedList].filter(verb => verb[selectedSearch].toLowerCase().startsWith(searchQuerry.toLowerCase().trim()))
-  }, [searchQuerry, sortedList, selectedSearch])
+    const sortedList = useMemo(() => {
+        if (selectedSort) {
+            return [...verbs].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+        } else {
+            return verbs;
+        }
+    }, [selectedSort, verbs]);
 
-  return (
-    <div className="App">
-      <div className="App__search">
-        <Search 
-          value={searchQuerry} 
-          onChange={(value: string) => setSearchQuerry(value)} 
-          placeholder={`Search verb in ${selectedSearch}`}
-        />
-        <Select onChange={(value:string) => setSelectedSearch(value)}/>
-      </div>
-      <table className="table">
-        <SortPanel sort={sortList} selectedSort={selectedSort}/>
-        <VerbsList verbs={sortedAndSearchedList}/>
-      </table>
-    </div>
-  );
+    const sortedAndSearchedList = useMemo(() => {
+        if (selectedSearch) {
+            return [...sortedList].filter(verb => verb[selectedSearch].toLowerCase().startsWith(searchQuerry.toLowerCase().trim()));
+        } else {
+            return verbs;
+        }
+    }, [searchQuerry, sortedList, selectedSearch])
+
+    return (
+        <div className="app">
+            <div className="app__search">
+                <Search
+                    value={searchQuerry}
+                    onChange={(value: string) => setSearchQuerry(value)}
+                    placeholder={`Search verb in ${selectedSearch}`}
+                />
+                <Select onChange={(value: string) => setSelectedSearch(value)} />
+            </div>
+            <div className="app__wrapper">
+                <table className="table">
+                    <SortPanel sort={sortList} selectedSort={selectedSort} />
+                    <VerbsList verbs={sortedAndSearchedList} />
+                </table>
+            </div>
+        </div>
+    );
 }
 
 export default App;
